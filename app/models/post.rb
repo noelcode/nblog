a35fba252bc88e505fa2bc4bfc3d65a0e3d0ecbf
Joe Zhou@aao <joezhou@msn.cn>
2013-02-13 22:31:32 +0800

    add resize support by default

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   attr_accessible :author, :picture, :text, :time, :title
   validates :title, :presence => true
 
-  has_attached_file :picture
+  has_attached_file :picture, :styles => { :original => '620x960>' }
 end
